4bd6ada1505e36f528e99aa48a995cdc8f688937
DJTheCreator <diegocajiao@outlook.com>
2024-10-21 18:28:26 -0400

    Downloader

--- commitData.py
+++ commitData.py
@@ -1,44 +1,60 @@
 import io
+import os
 
-import google.auth
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaIoBaseDownload
 
+SCOPES = ['https://www.googleapis.com/auth/drive']
+
+
+def getCreds():
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "credentials.json", SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+    return creds
+
 
 def download_file(real_file_id):
-  """Downloads a file
-  Args:
-      real_file_id: ID of the file to download
-  Returns : IO object with location.
+    try:
+        # create drive api client
+        service = build("drive", "v3", credentials=getCreds())
 
-  Load pre-authorized user credentials from the environment.
-  TODO(developer) - See https://developers.google.com/identity
-  for guides on implementing OAuth2 for the application.
-  """
-  creds, _ = google.auth.default()
+        file_id = real_file_id
 
-  try:
-    # create drive api client
-    service = build("drive", "v3", credentials=creds)
+        # pylint: disable=maybe-no-member
+        request = service.files().get_media(fileId=file_id)
+        file = io.BytesIO()
+        downloader = MediaIoBaseDownload(file, request)
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk()
+            print(f"Download {int(status.progress() * 100)}.")
 
-    file_id = real_file_id
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        file = None
 
-    # pylint: disable=maybe-no-member
-    request = service.files().get_media(fileId=file_id)
-    file = io.BytesIO()
-    downloader = MediaIoBaseDownload(file, request)
-    done = False
-    while done is False:
-      status, done = downloader.next_chunk()
-      print(f"Download {int(status.progress() * 100)}.")
-
-  except HttpError as error:
-    print(f"An error occurred: {error}")
-    file = None
-
-  return file.getvalue()
+    return file.getvalue()
 
 
 if __name__ == "__main__":
-  download_file(real_file_id="14nEvahpsxS3X-Pgk9nZTnpZxp3V-ByHWlD6KSTxdy78")
+    download_file(real_file_id="11JtWshzIBHbmbu385FVMjZ0U5wUa2Dry")
